f298a870965722ac1659b0ff9608dd0259f2ccf5
William <wlohier@stuy.edu>
2018-02-04 15:35:02 -0400

    Wrote fib and fibHelp functions to iterate through fib sequence

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -1,6 +1,8 @@
+import java.util.ArrayList;
+
 public class Recursion{
 
-    public static int fact(int n){
+    public static int fact(int n){ // basic factorial funcion, breaks down for larger numbers
 	if(n < 0){
 	    throw new IllegalArgumentException();
 	}
@@ -9,14 +11,40 @@ public class Recursion{
 	}
 	return(n * fact(n - 1));
     }
+    
 
+    public static int fib(int n){
+	if(n < 0){
+	    throw new IllegalArgumentException();
+	}
+	return fibHelp(0, 1, n);
+    }
+
+    public static int fibHelp(int t0, int t1, int n){
+	if(n == 0){
+	    return t0;
+	}
+	return fibHelp(t1, t0 + t1, n-1);
+    }
+
+
+
+
+
+    
     public static void main(String[] args){
 	//System.out.println(factorial(-2));
-	System.out.println(fact(0));
-	System.out.println(fact(1));
-	System.out.println(fact(5));
-        System.out.println(fact(11));
-	System.out.println(fact(12));
+	//	System.out.println(fact(0));
+	//	System.out.println(fact(1));
+	//	System.out.println(fact(5));
+	//        System.out.println(fact(11));
+	//	System.out.println(fact(12));
+	//System.out.println(fib(-1));
+	System.out.println(fib(0));
+	System.out.println(fib(1));
+	System.out.println(fib(10));
+	System.out.println(fib(20));
+	System.out.println(fib(30));
     }
 
 }
