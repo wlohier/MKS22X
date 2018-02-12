04e15d2544a2f031e8784cf5522ed82dffa3739b
William <wlohier@stuy.edu>
2018-02-11 23:41:43 -0400

    Wrote and tested add, remove, and toString

--- 02NKweens/QueenBoard.java
+++ 02NKweens/QueenBoard.java
@@ -10,22 +10,77 @@ public class QueenBoard{
 	}
     }
 
-    private boolean addQueen(int r, int c){
-	board[r][c] = -1;
-	for(inr row = r + 1; row < board.length; row++){
-	    if(r + 1 < board.length){
-		board[row][c]++
+    public boolean addKween(int r, int c){
+	if(board[r][c] == 0){
+	    board[r][c] = -1;
+	    for(int row = r + 1; row < board.length; row++){
+		if(r + 1 < board.length){
+		    board[row][c]++;
+	        }
 	    }
-        }
-        int diagUp = 1;
-	int diagDown = 1;
-	while(r + diagUp < board.length && c - diagUp >= 0){
-	    board[r + diagUp][c - diagUp]++;
-	    diagUp++;
+	    int diagUp = 1;
+	    int diagDown = 1;
+	    while(r + diagUp < board.length && c - diagUp >= 0){
+		board[r + diagUp][c - diagUp]++;
+		diagUp++;
+	    }
+	    while(r + diagDown < board.length && c + diagDown < board.length){
+		board[r + diagDown][c + diagDown]++;
+		diagDown++;
+	    }
+	    return true;
+	}return false;
+    }
+
+    public boolean removeKween(int r, int c){
+	if(board[r][c] == -1){
+	    board[r][c] = 0;
+	    for(int row = r + 1; row < board.length; row++){
+		if(r + 1 < board.length){
+		    board[row][c]--;
+	       	}
+	    }
+	    int diagUp = 1;
+	    int diagDown = 1;
+	    while(r + diagUp < board.length && c - diagUp >= 0){
+		board[r + diagUp][c - diagUp]--;
+		diagUp++;
+	    }
+	    while(r + diagDown < board.length && c + diagDown < board.length){
+		board[r + diagDown][c + diagDown]--;
+		diagDown++;
+	    }
+	    return true;
+	} return false;
+    }
+
+    public String toString(){
+	String ans = "";
+	for(int r = 0; r < board.length; r++){
+	    for(int c = 0; c < board[0].length; c++){
+		if(board[r][c] == -1){
+		    ans += "Q ";
+		}
+		else{
+		    ans += "_ ";
+		}
+	    } ans += "\n";
 	}
-	while(r + diagDown < board.length && c + diagDown < board.length){
-	    board[r + diagDown][c + diagDown]++;
-	    diagDown++;
+	return ans;
+    }
+
+     public String toStringAlt(){
+	String ans = "";
+	for(int c = 0; c < board.length; c++){
+	    for(int r = 0; r < board[0].length; r++){
+		if(board[r][c] == -1){
+		    ans += "Q ";
+		}
+		else{
+		    ans += board[r][c] + " ";
+		}
+	    } ans += "\n";
 	}
+	return ans;
     }
 }
