7a4a1f4f6c79676033ed525a9c9b2cfe575480e3
William Lohier <wlohier@stuy.edu>
2018-02-15 14:30:29 -0500

    Wrote first draft of solve and solveH hope it works!

--- 03KnightsOfNi/KnightBoard.java
+++ 03KnightsOfNi/KnightBoard.java
@@ -1,21 +1,18 @@
 public class KnightBoard{
+    private int[][] places = {{1, 2}, {1, -2}, {-1, 2}, {-1, -2},
+                              {2, 1}, {2, -1}, {-2, 1}, {-2, -1}};
     private int[][] board;
     public KnightBoard(int rows, int cols){
 	if(rows < 0 || cols < 0){
 	    throw new IllegalArgumentException();
 	}
 	board = new int[rows][cols];
-	for(int i = 0; i < board.length; i++){
-	    for(int j = 0; j < board[0].length; board++){
-		board[i][j] = 0;
-	    }
-	}
     }
 
     public String toString(){
 	String ans = "";
 	for(int r = 0; r < board.length; r++){
-	    for(c = 0; c < board[0].length; c++){
+	    for(int c = 0; c < board[0].length; c++){
 		if(board[r][c] == 0){
 		    ans += "_ ";
 		}
@@ -23,9 +20,9 @@ public class KnightBoard{
 		    ans += " " + board[r][c] + " ";
 		}
 		else{
-		    ans += 
+		    ans += board[r][c] + " ";
 		}
 	    }
-	}
+	} return ans;
     }
 }
