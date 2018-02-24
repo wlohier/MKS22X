25f29740db7d5f300c97dfe4e22d3a418ce36b0d
William <wlohier@stuy.edu>
2018-02-24 09:57:09 -0400

    Wrote preliminary solveHelp and finished toString

--- 03KnightsOfNi/KnightBoard.java
+++ 03KnightsOfNi/KnightBoard.java
@@ -8,45 +8,47 @@ public class KnightBoard{
 	}
 	board = new int[rows][cols];
 	for(int i = 0; i < board.length; i++){
-	    for(int j = 0; j < board[0].length; board++){
+	    for(int j = 0; j < board[0].length; j++){
 		board[i][j] = 0;
 	    }
 	}
     }
 
     public boolean solve(int startRow, int startCol){
+	return solveHelp(startRow, startCol, 1);
     }
 
     public boolean solveHelp(int startRow, int startCol, int num){
-	int thisRow = startRow + positions[i][0];
-	int thisCol = startCol + positions[i][1
 	if(num > board.length * board[0].length){
 	    return true;
 	}
 	for(int i = 0; i < positions.length; i++){
+	    int thisRow = startRow + positions[i][0];
+	    int thisCol = startCol + positions[i][1];
 	    if(board[thisRow][thisCol] == 0){
 		board[thisRow][thisCol] = num;
+		board[startRow][startCol] = num;
 		solveHelp(thisRow, thisCol, num+1);
-	    }
+	    } board[startRow][startCol] = 0;
 	}
-					      
-					      
+	return false;					      
     }
 
     public String toString(){
 	String ans = "";
 	for(int r = 0; r < board.length; r++){
-	    for(c = 0; c < board[0].length; c++){
+	    for(int c = 0; c < board[0].length; c++){
 		if(board[r][c] == 0){
 		    ans += "_ ";
 		}
-		else if(board.length *board[0].length >= 10 && board[r][c] < 10){
-		    ans += " " + board[r][c] + " ";
+		else if(board.length * board[0].length >= 10 && board[r][c] < 10){
+		    ans += "  " + board[r][c] + " ";
 		}
 		else{
-		    ans += 
+		    ans += " " + board[r][c] + " ";
 		}
 	    }
 	}
+	return ans;
     }
 }
