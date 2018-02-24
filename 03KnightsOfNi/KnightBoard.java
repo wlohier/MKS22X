15e5a787fadeef96a4a98a9425e48b85c762f143
William <wlohier@stuy.edu>
2018-02-24 10:13:40 -0400

    minor fixes on solveHelp. Added errors

--- 03KnightsOfNi/KnightBoard.java
+++ 03KnightsOfNi/KnightBoard.java
@@ -17,7 +17,7 @@ public class KnightBoard{
 
     public void checkBoard(){
 	for(int i = 0; i < board.length; i++){
-	    for(int j = 0; j < board.length; j++){
+	    for(int j = 0; j < board[0].length; j++){
 		if(board[i][j] != 0){
 		    throw new IllegalStateException();
 		}
@@ -35,13 +35,16 @@ public class KnightBoard{
 	    return true;
 	}
 	for(int i = 0; i < positions.length; i++){
-	    int thisRow = startRow + positions[i][0];
-	    int thisCol = startCol + positions[i][1];
-	    if(board[thisRow][thisCol] == 0){
-		board[thisRow][thisCol] = num;
-		board[startRow][startCol] = num;
-		solveHelp(thisRow, thisCol, num+1);
-	    } board[startRow][startCol] = 0;
+	    try{
+		int thisRow = startRow + positions[i][0];
+		int thisCol = startCol + positions[i][1];
+		if(board[thisRow][thisCol] == 0){
+		    board[startRow][startCol] = num;
+		    solveHelp(thisRow, thisCol, num+1);
+		} board[startRow][startCol] = 0;
+	    }
+	    catch(IndexOutOfBoundsException e){
+	    };
 	}
 	return false;					      
     }
