a9ec8db035777ca8b58b4a03514e1578298ec0af
William <wlohier@stuy.edu>
2018-03-02 15:50:13 -0400

    finished countSolutions gang gang

--- 03KnightsOfNi/KnightBoard.java
+++ 03KnightsOfNi/KnightBoard.java
@@ -31,7 +31,7 @@ public class KnightBoard{
 	return solveHelp(startRow, startCol, 1);
     }
 
-    public boolean solveHelp(int startRow, int startCol, int num){
+    private boolean solveHelp(int startRow, int startCol, int num){
 	board[startRow][startCol] = num;
 	if(num == board.length * board[0].length){
 	    return true;
@@ -39,9 +39,6 @@ public class KnightBoard{
 	for(int i = 0; i < positions.length; i++){
 	    int thisRow = startRow + positions[i][0];
 	    int thisCol = startCol + positions[i][1];
-	    //System.out.println(Text.go(1, 1));
-	    //System.out.println(this);
-	    //Text.wait(50); //adjust this delay
 	    if(thisRow < board.length && thisRow >= 0 &&
 	       thisCol < board[0].length && thisCol >= 0 &&
 	       board[thisRow][thisCol] == 0){
@@ -53,7 +50,31 @@ public class KnightBoard{
 	return false;
     }
 
-    public int countSolutions(int startRow, int startCol)
+    public int countSolutions(int startRow, int startCol){
+	checkBoard();
+	return countHelp(startRow, startCol, 1);
+    }
+
+    private int countHelp(int startRow, int startCol, int num){
+	int sum = 0;
+	if(num == board.length * board[0].length){
+	    return 1;
+	}
+	for(int i = 0; i < positions.length; i++){
+	    //System.out.println(Text.go(1, 1));
+	    //System.out.println(this);
+	    //Text.wait(20); //adjust this delay
+	    int thisRow = startRow + positions[i][0];
+	    int thisCol = startCol + positions[i][1];
+	    if(thisRow < board.length && thisRow >= 0 &&
+	       thisCol < board[0].length && thisCol >= 0 &&
+	       board[thisRow][thisCol] == 0){
+	        board[startRow][startCol] = num;
+	        sum += countHelp(thisRow, thisCol, num+1);		
+	    }
+	} board[startRow][startCol] = 0;
+	return sum;
+    }
 
     public String toString(){
 	String ans = "";
