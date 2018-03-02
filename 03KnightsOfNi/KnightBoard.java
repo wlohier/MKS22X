69ed71406fc8fbbfd59fa7abb6c86b273768df8b
William <wlohier@stuy.edu>
2018-03-02 15:27:02 -0400

    YES SOLVE WORKSSSSSSS

--- 03KnightsOfNi/KnightBoard.java
+++ 03KnightsOfNi/KnightBoard.java
@@ -1,7 +1,7 @@
 public class KnightBoard{
     private int[][] board;
     private int[][] positions = {{1, 2}, {1, -2}, {-1, 2}, {-1, -2},
-				 {2, 1}, {2, -1}, {-1, 2}, {-1, -2}};
+				 {2, 1}, {2, -1}, {-2, 1}, {-2, -1}};
     
     public KnightBoard(int rows, int cols){
 	if(rows < 0 || cols < 0){
@@ -32,36 +32,41 @@ public class KnightBoard{
     }
 
     public boolean solveHelp(int startRow, int startCol, int num){
-	if(num >= board.length * board[0].length-1){
+	board[startRow][startCol] = num;
+	if(num == board.length * board[0].length){
 	    return true;
 	}
 	for(int i = 0; i < positions.length; i++){
 	    int thisRow = startRow + positions[i][0];
 	    int thisCol = startCol + positions[i][1];
+	    //System.out.println(Text.go(1, 1));
+	    //System.out.println(this);
+	    //Text.wait(50); //adjust this delay
 	    if(thisRow < board.length && thisRow >= 0 &&
 	       thisCol < board[0].length && thisCol >= 0 &&
 	       board[thisRow][thisCol] == 0){
-		board[startRow][startCol] = num;
 		if(solveHelp(thisRow, thisCol, num+1)){
 		    return true;
 		}
-	    } board[startRow][startCol] = 0;
-	}
+	    }
+	} board[startRow][startCol] = 0;
 	return false;
     }
+
+    public int countSolutions(int startRow, int startCol)
 
     public String toString(){
 	String ans = "";
 	for(int r = 0; r < board.length; r++){
 	    for(int c = 0; c < board[0].length; c++){
 		if(board[r][c] == 0){
-		    ans += "_ ";
+		    ans += "  ";
 		}
-	        if(board.length * board[0].length >= 10){
+	        else if(board.length * board[0].length >= 10){
 		    if(board[r][c] > 9){
 			ans += board[r][c] + " ";
 		    }else{
-			ans += "_" + board[r][c] + " ";
+			ans += " " + board[r][c] + " ";
 		    }
 		}
 		else{
