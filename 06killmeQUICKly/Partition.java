479a15941c8a28aa8c4f33319765f262323bcc08
William Lohier <wlohier@stuy.edu>
2018-03-23 20:39:36 -0400

    partition works

--- 06killmeQUICKly/Partition.java
+++ 06killmeQUICKly/Partition.java
@@ -4,28 +4,21 @@ import java.util.*;
 public class Partition{
     
     public static int partition(int[] nums, int start, int end){
-	Random Ran = new Random();
-	int RI = java.lang.Math.abs((Ran.nextInt()%(end - start)))+start;
-	System.out.println(RI);
-	swap(nums, start, RI);
-	int temp = start;
-	start++;
+        int pivot = nums[end];
+	int temp = end;
+	end--;
         while(start < end){
-	    if(nums[start] > nums[temp]){
+	    if(nums[start] > pivot){
 	        swap(nums, start, end);
 		end--;
 	    }
 	    else{
 		start++;
 	    }
-	    System.out.println(Arrays.toString(nums));
 	}
-	int pivotTemp = nums[temp];
-	for(int i = 1; i < end; i++){
-	    nums[i-1] = nums[i];
-	}
-	nums[end] = pivotTemp;
-	return end;
+        swap(nums, temp, start + 1);
+	System.out.println(Arrays.toString(nums));
+	return start + 1;
     }
 
     public static void swap(int[] nums, int I1, int I2){
@@ -35,7 +28,7 @@ public class Partition{
     }
 
     public static void main(String[] args){
-	int[] nums = {999,999,999,4,1,0,3,2,999,999,999};
+	int[] nums = {999,999,999,4,1,0,3,2,999,999,50};
 	System.out.println(Partition.partition(nums, 0, 10));
 	//System.out.println(Arrays.toString(nums));
     }
