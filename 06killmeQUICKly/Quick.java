56840df1e61facc4f1b1aeb7a35982204d6a453e
William <wlohier@stuy.edu>
2018-03-25 23:57:01 -0300

    SMH quick still doesn't frigging work I don't know what the problem is

--- 06killmeQUICKly/Quick.java
+++ 06killmeQUICKly/Quick.java
@@ -2,12 +2,35 @@ import java.util.*;
 
 public class Quick{
 
-    public static quickSelect(int[] nums, int k){
-	int start = partition(nums, 0, nums.length-1);
-	if(start < k){
-	    while
+
+    public static void quicksort(int[] nums){
+        sort(nums, 0, nums.length-1);
+    }
+
+    public static void sort(int[] nums, int low, int high){
+	if(low < high){
+	    int k = partition(nums, low, high);
+	    sort(nums, k+1, high);
+	    sort(nums, low, k - 1);
 	}
     }
+
+    public static int quickselect(int[] nums, int k){
+        return select(nums, 0, nums.length-1, k);
+    }
+
+    public static int select(int[] nums, int low, int high, int k){
+	int start = partition(nums, low, high);
+	if(start < k){
+	    return select(nums, start+1, nums.length-1, k);
+	}
+	if(start > k){
+	    return select(nums, 0, start - 1, k);
+	}
+	return nums[start];
+    }
+
+    
     
     public static int partition(int[] nums, int start, int end){
         int pivot = nums[end];
@@ -21,15 +44,26 @@ public class Quick{
 	    else{
 		start++;
 	    }
+	    System.out.println(Arrays.toString(nums));
 	}
-        swap(nums, temp, start + 1);
+	
+        swap(nums, temp, start+1);
 	System.out.println(Arrays.toString(nums));
-	return start + 1;
+	return start+1;
     }
 
     public static void swap(int[] nums, int I1, int I2){
 	int temp = nums[I1];
 	nums[I1] = nums[I2];
 	nums[I2] = temp;
+    }
+
+    public static void main(String[] args){
+	int[] nums = {9,8,7,6,5,4,3,2,1,0};
+	System.out.println(Arrays.toString(nums));
+	//Quick.partition(nums, 0, 9);
+	//Quick.partition(nums, 1, 9);
+	Quick.quicksort(nums);
+        System.out.println(Arrays.toString(nums));
     }
 }
