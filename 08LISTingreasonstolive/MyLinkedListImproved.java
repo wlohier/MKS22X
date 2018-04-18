bd4d2a4821fb32dc028b81e06be5ebb7e4dd5d72
William Lohier <wlohier@stuy.edu>
2018-04-17 20:49:48 -0400

    Started making Generic linked list

---
+++ 08LISTingreasonstolive/MyLinkedListImproved.java
@@ -0,0 +1,181 @@
+public class MyLinkedListImproved<T>{
+    private int size;
+    private Node first;
+    private Node last;
+    public MyLinkedList(){
+	size = 0;
+	first = null;
+	last = null;
+    }
+
+    public boolean add(T i){
+	Node n = new Node(i);
+	last.setNext(n);
+	last = n;
+	size++;
+	return true;
+    }
+
+    public void clear(){
+	first = null;
+	last = null;
+	size = 0;
+    }
+
+    public boolean add(int index, T val){
+	if(index >= size || index < 0){
+	    throw new IndexOutOfBoundsException();
+	}
+	
+        if(index == size()){
+	    add(val);
+	    return true;
+	}
+	else if(index == 0){
+	    Node n = new Node(val);
+	    n.setNext(first);
+	    first = n;
+	    size++;
+	    return true;
+	}
+        else{
+	    Node newNode = new Node(val);
+	    newNode.setNext(getNode(index));
+	    newNode.setPrev(getNode(index-1));
+	    getNode(index-1).setNext(newNode);
+	    getNode(index+1).setPrev(newNode);
+	    size++;
+	    return true;
+	}
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public T get(int index){
+	if(index > size()-1 || index < 0){
+	    throw new IndexOutOfBoundsException();
+	}
+	return getNode(index).getVal();
+    }
+
+    public T set(int index, Integer value){
+	if(index >= size() || index < 0){
+	    throw new IndexOutOfBoundsException();
+	}
+        T i = getNode(index).getVal();
+	getNode(index).setVal(value);
+	return i;
+    }
+
+    public int indexOf(T value){
+	int i = 0;
+	Node n = first;
+	while(i < size()){
+	    if(n.getVal().equals(value)){
+		return i;
+	    }
+	    i++;
+	    n = n.getNext();
+	} return -1;
+    }
+
+    public boolean remove(T value){
+	Node n = getNode(value);
+	if(n.equals(null)){
+	    return false;
+	}
+        n.getPrev().setNext(n.getNext());
+	n.getNext().setPrev(n.getPrev());
+	return true;
+    }
+
+    public T remove(int index){
+	if(index >= size() || index < 0){
+	    throw new IndexOutOfBoundsException();
+	}
+	
+	Node n = getNode(index);
+        T i = n.getVal();
+	n.getPrev().setNext(n.getNext());
+	n.getNext().setPrev(n.getPrev());
+	return i;
+    }
+
+    private Node getNode(int index){
+	int i = 0;
+	Node n = first;
+        while(i < index){
+	    n = n.getNext();
+	    i++;
+	}
+	return n;
+    }
+
+    private Node getNode(T val){
+	int i = 0;
+	Node n = first;
+	while(i < size()){
+	    if(n.getVal().equals(val)){
+		return n;
+	    }
+	    i++;
+	    n = n.getNext();
+	}
+	return null;
+    }
+
+    public String toString(){
+	String ans = "[";
+	int i = 0;
+	Node n = first;
+        while(i < size()){
+	    ans += n.toString();
+	    ans += ", ";
+	    n = n.getNext();
+	    i++;
+	}
+	ans = ans.substring(0, ans.length()-2);
+	ans += "]";
+	return ans;
+    }
+
+    private class Node{
+	private Node next,prev;
+	private T data;
+	private Node(T i){
+	    data = i;
+	    next = null;
+	    prev = null;
+	}
+
+	private Node getNext(){
+	    return next;
+	}
+
+	private void setNext(Node n){
+	    next = n;
+	}
+
+	private Node getPrev(){
+	    return prev;
+	}
+
+	private void setPrev(Node n){
+	    prev = n;
+	}
+
+	private T getVal(){
+	    return data;
+	}
+
+	private void setVal(int i){
+	    data = i;
+	}
+
+	public String toString(){
+	    return "" + data;
+	}
+    }
+}
