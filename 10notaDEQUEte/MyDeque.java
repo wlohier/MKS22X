823e4304ca46f787feb5aa7bce29614625891963
William <wlohier@stuy.edu>
2018-05-02 02:11:18 -0300

    Worked on Deque

--- 10notaDEQUEte/MyDeque.java
+++ 10notaDEQUEte/MyDeque.java
@@ -1,9 +1,38 @@
 public class MyDeque<E>{
     private E[] data;
+    private int size;
+    private int start;
+    private int end;
+    
     public MyDeque(){
 	data = new E[10];
+	size = 0;
+	start = 0;
+	end = 0;
     }
     public MyDeque(int init){
 	data = new E[init];
+	size = 0;
+	start = 0;
+	end = 0;
     }
+
+    public int size(){
+	return size;
+    }
+
+    private int[] resize(E[] data){
+        E[] newdata = new E[data.length*2];
+	for(int i = 0; i < size(); i++){
+	    newdata[i] = data[(i+start)%size()];
+	}
+	start = 0;
+	end = size();
+	return newdata;
+    }
+
+    public void addFirst(E val){
+	
+    }
+    
 }
