21ff5969c6e4911d5ff870ad1f451a1448d1f557
William Lohier <wlohier@stuy.edu>
2018-05-07 19:59:32 -0400

    Finished all Deque methods

--- 10notaDEQUEte/MyDeque.java
+++ 10notaDEQUEte/MyDeque.java
@@ -1,17 +1,25 @@
+import java.util.*;
+
 public class MyDeque<E>{
     private E[] data;
     private int size;
     private int start;
     private int end;
-    
+
+    @SuppressWarnings("unchecked")
     public MyDeque(){
-	data = new E[10];
+        data = (E[])new Object[10];
 	size = 0;
 	start = 0;
 	end = 0;
     }
+
+    @SuppressWarnings("unchecked")
     public MyDeque(int init){
-	data = new E[init];
+	if(init < 0){
+	    throw new IllegalArgumentException();
+	}
+	data = (E[])new Object[init];
 	size = 0;
 	start = 0;
 	end = 0;
@@ -21,48 +29,75 @@ public class MyDeque<E>{
 	return size;
     }
 
-    private void resize(E[] data){
-        E[] newdata = new E[data.length*2];
+    @SuppressWarnings("unchecked")
+    private void resize(){
+        E[] newdata = (E[])new Object[data.length*2];
 	for(int i = 0; i < size(); i++){
 	    newdata[i] = data[(i+start)%size()];
 	}
 	start = 0;
 	end = size();
-        data = newData;
+        data = newdata;
     }
 
     public void addFirst(E val){
+	if(val.equals(null)){
+	    throw new IllegalArgumentException();
+	}
 	if((start-1)%(data.length) == end){
 	    resize();
 	}
 	data[(start-1)%data.length] = val;
-	first = (start-1)%data.length;
+	start = (start-1)%data.length;
 	size++;
     }
 
     public void addLast(E val){
+	if(val.equals(null)){
+	    throw new IllegalArgumentException();
+	}
 	if((end+1)%(data.length)==start){
 	    resize();
 	}
 	data[(end+1)%data.length] = val;
-	last = (end+1)%data.length;
+	end = (end+1)%data.length;
 	size++;
     }
 
     public E removeFirst(){
-	E ans = data[first];
-	data[first] = null;
+	if(size()==0){
+	    throw new NoSuchElementException();
+	}
+	E ans = data[start];
+	data[start] = null;
 	size--;
-	first = (first+1)%data.length;
+	start = (start+1)%data.length;
 	return ans;
     }
 
     public E removeLast(){
-	E ans = data[last];
-	data[last] = null;
-	last = (end-1)%data.length;
+	if(size()==0){
+	    throw new NoSuchElementException();
+	}
+	E ans = data[end];
+	data[end] = null;
+	end = (end-1)%data.length;
 	size--;
 	return ans;
+    }
+
+    public E getFirst(){
+	if(size()==0){
+	    throw new NoSuchElementException();
+	}
+	return data[start];
+    }
+
+    public E getLast(){
+	if(size()==0){
+	    throw new NoSuchElementException();
+	}
+	return data[end];
     }
     
 }
