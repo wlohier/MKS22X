f4e92b0ee6b2f375dbcce442946c8d58bde95926
William <wlohier@stuy.edu>
2018-05-08 20:12:25 -0300

    Wrote a heap?? cool beans

--- 12aHEAPofwork/MyHeap.java
+++ 12aHEAPofwork/MyHeap.java
@@ -1,13 +1,16 @@
-public class MyHeap<E extends comparable<E>>{
+public class MyHeap<E extends Comparable<E>>{
     private E[] data;
     private int size;
     private boolean MAX;
 
+    @SuppressWarnings("unchecked")
     public MyHeap(){
-	data = new E[10];
+	data = (E[])new Object[10];
 	size = 0;
+	MAX = true;
     }
 
+    @SuppressWarnings("unchecked")
     public MyHeap(boolean b){
 	if(b){
 	    MAX = true;
@@ -15,7 +18,7 @@ public class MyHeap<E extends comparable<E>>{
 	else{
 	    MAX = false;
 	}
-	data = new E[10];
+	data = (E[])new Object[10];
 	size = 0;
     }
 
@@ -33,8 +36,21 @@ public class MyHeap<E extends comparable<E>>{
 	size++;
     }
 
+    public String remove(){
+	String ans = data[0].toString();
+	data[0] = data[size-1];
+	if(MAX){
+	    pushDMax(data[0], 0);
+	}
+	else{
+	    pushDMin(data[0], 0);
+	}
+	size--;
+	return ans;
+    }
+
     private void pushMin(E val, int index){
-	compInd = (index-1)/2;
+	int compInd = (index-1)/2;
         if(compInd > 0){
 	    if(val.compareTo(data[compInd]) < 0){
 		swap(index, compInd);
@@ -43,8 +59,8 @@ public class MyHeap<E extends comparable<E>>{
 	}
     }
 
-    private void pushMax(E val){
-	compInd = (index-1)/2;
+    private void pushMax(E val, int index){
+	int compInd = (index-1)/2;
         if(compInd > 0){
 	    if(val.compareTo(data[compInd]) >= 0){
 		swap(index, compInd);
@@ -53,8 +69,42 @@ public class MyHeap<E extends comparable<E>>{
 	}
     }
 
+    private void pushDMax(E val, int index){
+	int compInd1 = index*2+1;
+	int compInd2 = index*2+2;
+	int comp = compInd1;
+	if(data[compInd1].compareTo(data[compInd2]) < 0){
+	    comp = compInd2;
+	}
+	if(comp < size){
+	    if(val.compareTo(data[comp]) < 0){
+		swap(index, comp);
+		pushDMax(val, comp);
+	    }
+	}
+    }
+
+    private void pushDMin(E val, int index){
+	int compInd1 = index*2+1;
+	int compInd2 = index*2+2;
+	int comp = compInd1;
+	if(data[compInd1].compareTo(data[compInd2]) > 0){
+	    comp = compInd2;
+	}
+	if(comp < size){
+	    if(val.compareTo(data[comp]) > 0){
+		swap(index, comp);
+		pushDMin(val, comp);
+	    }
+	}
+    }
+
     public int size(){
 	return size;
+    }
+
+    public String peek(){
+	return data[0].toString();
     }
 
     private void swap(int ind1, int ind2){
@@ -63,8 +113,9 @@ public class MyHeap<E extends comparable<E>>{
 	data[ind2] = temp;
     }
 
+    @SuppressWarnings("unchecked")
     private void resize(){
-	E[] newdata = new E[size*2];
+	E[] newdata = (E[])new Object[size*2];
 	for(int i = 0; i < size; i++){
 	    newdata[i] = data[i];
 	}
