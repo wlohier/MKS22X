c94e9cf534aa07f667ec5905b2d68fe1c4467add
William <wlohier@stuy.edu>
2018-05-10 02:14:09 -0300

    Finished heapsort methods

--- 12aHEAPofwork/Sorts.java
+++ 12aHEAPofwork/Sorts.java
@@ -1,14 +1,21 @@
 public class Sorts{
     public static void heapSort(int[] data){
+	heapify(data);
+	int part = data.length-1;
+	for(int i = 0; i < data.length; i++){
+	    swap(data, 0, part);
+	    pushDMax(data, data[0], 0);
+	    part--;
+	}
     }
 
     public static void heapify(int[]data){
 	for(int i = data.length-1; i >= 0; i--){
-	    pushDMax(data[i], i);
+	    pushDMax(data, data[i], i);
 	}
     }
 
-    private static void pushDMax(int val, int index){
+    public static void pushDMax(int[] data, int val, int index){
 	int compInd1 = index*2+1;
 	int compInd2 = index*2+2;
 	int comp = compInd1;
@@ -17,13 +24,13 @@ public class Sorts{
 	}
 	if(comp < data.length){
 	    if(val < data[comp]){
-		swap(index, comp);
-		pushDMax(val, comp);
+		swap(data, index, comp);
+		pushDMax(data, val, comp);
 	    }
 	}
     }
 
-    private void swap(int ind1, int ind2){
+    public static  void swap(int[] data, int ind1, int ind2){
 	int temp = data[ind1];
 	data[ind1] = data[ind2];
 	data[ind2] = temp;
