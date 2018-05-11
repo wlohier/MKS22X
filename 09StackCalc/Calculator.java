823b6286d8ffd1f394308039c2a270979166bafe
William Lohier <wlohier@stuy.edu>
2018-05-10 20:50:08 -0400

    Tested StackCalc

--- 09StackCalc/Calculator.java
+++ 09StackCalc/Calculator.java
@@ -7,22 +7,28 @@ public class Calculator{
 		    Double.parseDouble(data[i-2]) + "";
 	    }
 	    if(data[i].equals("-")){
-		data[i] = Double.parseDouble(data[i-1])-
-		    Double.parseDouble(data[i-2]) + "";
+		data[i] = Double.parseDouble(data[i-2])-
+		    Double.parseDouble(data[i-1]) + "";
 	    }
 	    if(data[i].equals("*")){
 		data[i] = Double.parseDouble(data[i-1])*
 		    Double.parseDouble(data[i-2]) + "";
 	    }
 	    if(data[i].equals("/")){
-		data[i] = Double.parseDouble(data[i-1])/
-		    Double.parseDouble(data[i-2]) + "";
+		data[i] = Double.parseDouble(data[i-2])/
+		    Double.parseDouble(data[i-1]) + "";
 	    }
 	    if(data[i].equals("%")){
-		data[i] = Double.parseDouble(data[i-1])%
-		    Double.parseDouble(data[i-2]) + "";
+		data[i] = Double.parseDouble(data[i-2])%
+		    Double.parseDouble(data[i-1]) + "";
 	    }
 	}
 	return Double.parseDouble(data[data.length-1]);
+    }
+
+    public static void main(String[] args){
+	System.out.println(eval("10 2.0 +"));
+	System.out.println(eval("11 3 - 4 + 2.5 *"));
+	System.out.println(eval("8 2 + 99 9 - * 2 + 9 -"));
     }
 }
