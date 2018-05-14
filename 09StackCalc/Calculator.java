40b8c828b9c7595fe45f1f9180a6135477530cc7
William <wlohier@stuy.edu>
2018-05-14 00:38:24 -0300

    Fixed StackCalc for more complicated input

--- 09StackCalc/Calculator.java
+++ 09StackCalc/Calculator.java
@@ -3,27 +3,50 @@ public class Calculator{
 	String[] data = s.split(" ");
 	for(int i = 0; i < data.length; i++){
 	    if(data[i].equals("+")){
+		pushUp(i, data);
 		data[i] = Double.parseDouble(data[i-1])+
 		    Double.parseDouble(data[i-2]) + "";
+		data[i-1] = null;
+		data[i-2] = null;
 	    }
 	    if(data[i].equals("-")){
+		pushUp(i, data);
 		data[i] = Double.parseDouble(data[i-2])-
 		    Double.parseDouble(data[i-1]) + "";
+		data[i-1] = null;
+		data[i-2] = null;
 	    }
 	    if(data[i].equals("*")){
+		pushUp(i, data);
 		data[i] = Double.parseDouble(data[i-1])*
 		    Double.parseDouble(data[i-2]) + "";
+		data[i-1] = null;
+		data[i-2] = null;
 	    }
 	    if(data[i].equals("/")){
+		pushUp(i, data);
 		data[i] = Double.parseDouble(data[i-2])/
 		    Double.parseDouble(data[i-1]) + "";
+		data[i-1] = null;
+		data[i-2] = null;
 	    }
 	    if(data[i].equals("%")){
+		pushUp(i, data);
 		data[i] = Double.parseDouble(data[i-2])%
 		    Double.parseDouble(data[i-1]) + "";
+		data[i-1] = null;
+		data[i-2] = null;
 	    }
 	}
 	return Double.parseDouble(data[data.length-1]);
+    }
+
+    public static void pushUp(int i, String[] data){
+        int less = 1;
+	while(data[i-2] == null){
+	    data[i-2] = data[i-2-less];
+	    less++;
+	}
     }
 
     public static void main(String[] args){
